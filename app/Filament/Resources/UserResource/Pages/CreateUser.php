5cea2977b1e0770ddb77e80239e0e13562bc72c3
nicholaschun <nicholaschunrayne@gmail.com>
2023-09-25 03:05:11 +0000

    add member crud

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -3,10 +3,66 @@
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Member registered')
+            ->body('Member has been created successfully.');
+    }
+    protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
+
+    public function form(Form $form): Form
+  {
+        return $form
+            ->schema([
+                Select::make('title')
+                    ->required()
+                    ->options([
+                        'Mr.' => 'Mr.',
+                        'Mrs' => 'Mrs',
+                        'Miss' => 'Miss',
+                    ]),
+                TextInput::make('first_name')->required(),
+                TextInput::make('middle_name'),
+                TextInput::make('last_name')->required(),
+                TextInput::make('gender')->required(),
+                DateTimePicker::make('employment_date')->required(),
+                DateTimePicker::make('join_ggssa_date')->required(),
+                TextInput::make('gngc_staff_number')->required(),
+                TextInput::make('department')->required(),
+                TextInput::make('gngc_job_title')->required(),
+                TextInput::make('workstation')->required(),
+                TextInput::make('contact_number')->required(),
+                TextInput::make('whatsapp_number')->required(),
+                TextInput::make('gngc_email')->required(),
+                TextInput::make('marital_status')->required(),
+                TextInput::make('number_of_children')->required(),
+                TextInput::make('religion')->required(),
+                TextInput::make('emergency_contact')->required()
+            ]);
+    }  
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['password'] = $data['first_name'] . '@1234';
+        $data['status'] = true;
+        $data['email'] = $data['gngc_email'];
+        $data['name'] = $data['first_name'] . ' ' . $data['middle_name'] . ' ' . $data['last_name'];
+        return $data;
+    }
 }
--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,6 +5,11 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
 
 class EditUser extends EditRecord
 {
@@ -13,7 +18,38 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
         ];
     }
+
+    public function form(Form $form): Form
+    {
+          return $form
+              ->schema([
+                  Select::make('title')
+                      ->required()
+                      ->options([
+                          'Mr.' => 'Mr.',
+                          'Mrs' => 'Mrs',
+                          'Miss' => 'Miss',
+                      ]),
+                  TextInput::make('first_name')->required(),
+                  TextInput::make('middle_name'),
+                  TextInput::make('last_name')->required(),
+                  TextInput::make('gender')->required(),
+                  DateTimePicker::make('employment_date')->required(),
+                  DateTimePicker::make('join_ggssa_date')->required(),
+                  TextInput::make('gngc_staff_number')->required(),
+                  TextInput::make('department')->required(),
+                  TextInput::make('gngc_job_title')->required(),
+                  TextInput::make('workstation')->required(),
+                  TextInput::make('contact_number')->required(),
+                  TextInput::make('whatsapp_number')->required(),
+                  TextInput::make('gngc_email')->required(),
+                  TextInput::make('marital_status')->required(),
+                  TextInput::make('number_of_children')->required(),
+                  TextInput::make('religion')->required(),
+                  TextInput::make('emergency_contact')->required()
+              ]);
+      }  
 }
