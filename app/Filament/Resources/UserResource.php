d120d0a5398d66e8010536b75d25663bd56c2127
nicholaschun <nicholaschunrayne@gmail.com>
2023-09-25 01:53:23 +0000

    fix table column

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,16 +3,12 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
-use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Filament\Resources\Tables\Columns;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 
 class UserResource extends Resource
 {
@@ -34,15 +30,15 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                Columns\Text::make('id')->primary(),
-                Columns\Text::make('name')->label('Full Name'),
-                Columns\Text::make('gender')->label('Gender'),
-                Columns\Text::make('employment_date')->date()->label('Employment Date'),
-                Columns\Text::make('gngc_staff_number')->label('Staff Number'),
-                Columns\Text::make('department')->label('Department'),
-                Columns\Text::make('gngc_job_title')->label('Job Title'),
-                Columns\Text::make('date_of_birth')->label('Birthday'),
-                Columns\Text::make('contact_number')->label('Contact Number'),
+                Tables\Columns\TextColumn::make('id')->label('id'),
+                Tables\Columns\TextColumn::make('name')->label('Full Name'),
+                Tables\Columns\TextColumn::make('gender')->label('Gender'),
+                Tables\Columns\TextColumn::make('employment_date')->date()->label('Employment Date'),
+                Tables\Columns\TextColumn::make('gngc_staff_number')->label('Staff Number'),
+                Tables\Columns\TextColumn::make('department')->label('Department'),
+                Tables\Columns\TextColumn::make('gngc_job_title')->label('Job Title'),
+                Tables\Columns\TextColumn::make('date_of_birth')->label('Birthday'),
+                Tables\Columns\TextColumn::make('contact_number')->label('Contact Number'),
             ])
             ->filters([
                 //
